061f6a6d1ced08f340edb4f3b91934f562e6b1ed
Vacip Derici <dericivd@gmail.com>
2022-07-29 18:00:35 +0300

    Demo Using HEAD and OPTION

--- src/main/java/com/psrestassured/AnotherUser.java
+++ src/main/java/com/psrestassured/AnotherUser.java
@@ -5,12 +5,10 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class User {
+public class AnotherUser {
 
     public String login;
     public  int id;
-
-    @JsonProperty("public_repos")
     public  int publicRepos;
 
 
--- src/test/java/com/preassured/m6objectmapping/ObjectMappingDemo.java
+++ src/test/java/com/preassured/m6objectmapping/ObjectMappingDemo.java
@@ -1,22 +1,33 @@
 package com.preassured.m6objectmapping;
 
+import com.psrestassured.AnotherUser;
 import com.psrestassured.User;
 import io.restassured.RestAssured;
+import io.restassured.internal.mapping.Jackson2Mapper;
+import io.restassured.mapper.ObjectMapper;
+import io.restassured.mapper.ObjectMapperDeserializationContext;
+import io.restassured.mapper.ObjectMapperSerializationContext;
+import io.restassured.mapper.ObjectMapperType;
 import org.testng.annotations.Test;
+
+import java.lang.reflect.Type;
 
 import static org.testng.Assert.assertEquals;
 
 public class ObjectMappingDemo {
 
-    public  static final String BASE_URL ="https://api.github.com/users/rest-assured";
+    public static final String BASE_URL = "https://api.github.com/users/rest-assured";
 
     @Test
-    public void ObjectMappingTestOne(){
-         User user =   RestAssured.get(BASE_URL)
+    public void ObjectMappingTestOne() {
+        User user = RestAssured.get(BASE_URL)
                 .as(User.class);
 
-            assertEquals(user.getLogin(),"rest-assured");
-            assertEquals(user.getId(),"rest-assured");
+        assertEquals(user.getLogin(), "rest-assured");
+        assertEquals(user.getId(), 19369327);
+        assertEquals(user.getPublicRepos(), 2);
 
     }
+
+
 }
